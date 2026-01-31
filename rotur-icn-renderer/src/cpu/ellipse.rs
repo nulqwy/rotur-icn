57c21bf9d5d80c13155aaa0cf8dcb8e0b373780d
nulqwy <ivan.demian2009@gmail.com>
2026-02-01 00:47:49 +0200

    use a proper ellipse formula

--- rotur-icn-renderer/src/cpu/ellipse.rs
+++ rotur-icn-renderer/src/cpu/ellipse.rs
@@ -1,24 +1,30 @@
 use rotur_icn_compiler::resolver::lir;
 use rotur_icn_units::Vector;
 
-// FIXME technically this is a rather wrong approximation
-pub fn distance(el: &lir::Ellipse, pos: Vector) -> f32 {
-    let ratio = el.sizes.y / el.sizes.x;
+// from https://www.shadertoy.com/view/tt3yz7
+pub fn distance_sq(el: &lir::Ellipse, pos: Vector) -> f32 {
+    let e = el.sizes;
+    let p_abs = (pos - el.centre).rotate(-el.direction).abs();
+    let ei = 1. / e;
+    let e2 = e.powi(2);
+    let ve = ei
+        * Vector {
+            x: e2.x - e2.y,
+            y: e2.y - e2.x,
+        };
 
-    let rel_pos = (pos - el.centre).rotate(-el.direction);
+    let mut t = Vector::new_normal(std::f32::consts::FRAC_PI_4);
+    for _ in 0..3 {
+        let v = ve * t.powi(3);
+        let u = (p_abs - v).normalise() * (t * e - v).length();
+        let w = ei * (v + u);
+        t = w.clamp(0., 1.).normalise();
+    }
 
-    let skewed = Vector {
-        x: rel_pos.x,
-        y: rel_pos.y / ratio,
-    };
-
-    let d = (skewed.length() - el.sizes.x).abs();
-
-    let factor = skewed.angle().sin().abs();
-
-    d * (1. + factor * (ratio - 1.))
+    let nearest_abs = t * e;
+    (p_abs - nearest_abs).length_sq()
 }
 
 pub fn test(el: &lir::Ellipse, pos: Vector) -> bool {
-    distance(el, pos) <= el.outline_width / 2.
+    distance_sq(el, pos) <= el.outline_width * el.outline_width / 4.
 }
